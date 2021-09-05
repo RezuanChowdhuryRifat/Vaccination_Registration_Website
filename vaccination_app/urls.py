5bc9d81ba91cb1210e0da4f241dc994ab4b4853d
Yash-Haque <58705635+Yash-Haque@users.noreply.github.com>
2021-09-05 22:47:24 +0600

    Update urls.py

--- vaccination_app/urls.py
+++ vaccination_app/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from .views import AddressView,RegistrationView,VaccinecardView
+from . import views
 
 app_name = 'vaccination_app'
 
 urlpatterns = [
 
-    path('centeraddress/', AddressView.as_view(), name = 'centerAddress'),
-    path('registration/', RegistrationView.as_view(), name = 'registration'),
-    path('vaccinecard/', VaccinecardView.as_view(), name = 'vaccinationcard'),
+    path('home/', views.HomePageView, name='HomePageView'),
+    path('covidcheck/', views.CovidCheckView, name='CovidCheckView'),
+    path('faq/', views.FaqView, name='FaqView'),
+
+    path('centeraddress/', AddressView.as_view(), name='centerAddress'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('vaccinecard/', VaccinecardView.as_view(), name='vaccinationcard'),
    
 ]

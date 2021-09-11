951797f230e8c9f191360470ec4e856c453f4259
RezuanChowdhuryRifat <87327764+RezuanChowdhuryRifat@users.noreply.github.com>
2021-09-11 07:09:18 +0600

    deployment setup

--- Vaccination_registration/settings.py
+++ Vaccination_registration/settings.py
@@ -25,9 +25,9 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY =os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 MEDIA_URL ='/media/'
@@ -55,6 +55,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
